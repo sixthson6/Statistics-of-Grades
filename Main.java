38bb80bef5046708e07309eff464e28fdf3eb6c6
sixthson6 <sudais2408@gmail.com>
2025-04-03 10:19:10 -0700

    calculate statistics; max, min, avg. initialize graph

--- Main.java
+++ Main.java
@@ -1,21 +1,24 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> scores = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
+        Data data = new Data();
+        Statistics statistics = new Statistics(data);
 
-        System.out.println("Enter number of scores(N): ");
-        int n = scanner.nextInt();
+        data.collectData();
+        // statistics.printData();
 
-        System.out.println("Enter Grades: ");
-        for (int i = 0; i < n; i++) {
-            scores.add(scanner.nextInt());
-        }
+        int maxGradeNum = statistics.maxGrade();
+        int minGradeNum = statistics.minGrade();
+        double avgNum = statistics.computeAvg();
+        Graph graph = new Graph();
+        int[] stats = graph.stats();
+        System.out.println("The maximum grade is " + maxGradeNum);
+        System.out.println("The minimum grade is " + minGradeNum);   
+        System.out.println("The average grade is " + avgNum);
 
-        System.out.println(scores);
-        scanner.close();
-        
+        System.out.println("");
+        System.out.println(Arrays.toString(stats));
+        // statistics.minGrade();
     }
 }
